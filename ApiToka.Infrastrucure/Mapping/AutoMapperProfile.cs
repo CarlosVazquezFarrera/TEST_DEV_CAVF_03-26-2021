e270a20ee116d90230e3cb997a4487e3de09a378
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2021-03-25 19:20:22 -0600

    AutoMapper agregado

---
+++ ApiToka.Infrastrucure/Mapping/AutoMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿namespace ApiToka.Infrastrucure.Mapping
+{
+    using ApiToka.Core.DTOs;
+    using ApiToka.Core.Entites;
+    using AutoMapper;
+    public class AutoMapperProfile: Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<TbPersonasFisicas, TbPersonasFisicasDTO>();
+            CreateMap<TbPersonasFisicasDTO, TbPersonasFisicas>();
+        }
+
+    }
+}
